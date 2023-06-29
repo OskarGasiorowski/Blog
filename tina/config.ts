55265f7b27f1a4bd93ed2e23acc5f42dbd47c3bf
OskarGasiorowski <oskar@gasiorowski.ovh>
2023-06-29 11:56:51 +0200

    updated tina config

--- tina/config.ts
+++ tina/config.ts
@@ -5,8 +5,8 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main';
 
 export default defineConfig({
     branch,
-    clientId: 'n', // Get this from tina.io
-    token: null, // Get this from tina.io
+    clientId: 'ab53adba-562b-49a8-9d06-32a03b2d6a84', // Get this from tina.io
+    token: process.env.TINA_TOKEN || '', // Get this from tina.io
 
     build: {
         outputFolder: 'admin',
